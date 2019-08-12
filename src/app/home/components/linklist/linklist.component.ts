9f75626d4927b2c536399a62ca62b2335c78cc18
jiangyupeng <jiangyupeng@inspur.com>
2019-08-12 17:33:24 +0800

    finish all with localstorage 5M

--- src/app/home/components/linklist/linklist.component.ts
+++ src/app/home/components/linklist/linklist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {StorageService} from '../../../core/services/electron/storage.service';
 
 @Component({
   selector: 'app-linklist',
@@ -11,23 +12,32 @@ export class LinklistComponent implements OnInit {
   public link_key: string;
   public link_value: string;
 
-  constructor() { }
+  constructor(public storage: StorageService) { }
 
   ngOnInit() {
+    let getdata = this.storage.get('linklist');
+    if (getdata) {
+      this.link_list = getdata;
+    }
   }
 
-  add(e: any) {
-    if (e.keyCode == 13) {
-      this.link_list.push(
-        {
-          key: this.link_key,
-          value: this.link_value
-        }
-      );
+  add(value: string) {
+    
+    this.link_list.push(
+      {
+        key: this.link_key,
+        value: value
+      }
+    );
+    this.storage.set('linklist', this.link_list);
 
-      this.link_key = '';
-      this.link_value = '';
-    }
+    this.link_key = '';
+    this.link_value = '';
+  }
+
+  delete(key: any) {
+    this.link_list.splice(key, 1);
+    this.storage.set('linklist', this.link_list);
   }
 
 }
