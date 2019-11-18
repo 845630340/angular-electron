ae29b414a110764b4980ea2837ec37c85ff545ab
jiangyupeng <jiangyupeng@inspur.com>
2019-11-18 09:44:45 +0800

    finish show_list func

--- src/app/home/components/note/note.component.ts
+++ src/app/home/components/note/note.component.ts
@@ -11,29 +11,38 @@ export class NoteComponent implements OnInit {
   public page = 1;  // 根据左边button变换页面内容
   public time: Date;  // edit文章的保存时间
   public isShowTime: boolean = false;
-  public content: string;  // edit编辑框中，当前显示的内容
+  public content: string = this.noteService.get().__wrapped__.note[0].post;  // edit编辑框中，当前显示的内容
+  public count = this.noteService.get().__wrapped__.note.length;
+  public data_list: any = [];
 
   public page2_data: string;
 
   constructor(public noteService: NoteLowdbService) { }
   ngOnInit() {
-    
+    // console.log(this.noteService.get())
+    // console.log(this.noteService.get().__wrapped__.note[0].post)
   }
-  // ------------------------功能1-------------------------
+  // ------------------------功能 1-------------------------
   edit() {
     this.page = 1;
   }
   save() {
-    this.time = new Date();
-    this.isShowTime = true;
-    this.page2_data = this.content;
+    if (this.content) {
+      this.time = new Date();
+      this.isShowTime = true;
 
-    let data = {
-      post: this.content,
-      time: this.time
+      let data = {
+        post: this.content,
+        time: this.time
+      }
+      this.noteService.set(data);
+      this.data_list = this.noteService.get();
+      this.count = this.noteService.get().__wrapped__.note.length;;
+      alert('已保存。')
+    } else {
+      alert('还未输入任何内容。')
     }
-    console.log("data is:", data);
-    this.noteService.set(data);
+    
   }
 
   reset() {
@@ -41,11 +50,24 @@ export class NoteComponent implements OnInit {
     this.isShowTime = false;
   }
 
-  // ------------------------功能2-------------------------
+  // ------------------------功能 2-------------------------
   show_list() {
     this.page = 2;
+    this.data_list = this.noteService.get();
   }
 
+  delete(item: any) {
+    this.noteService.remove_note(item);
+    this.data_list = this.noteService.get();
+    this.count = this.noteService.get().__wrapped__.note.length;
+  }
+
+  show_detail(item: any) {
+    this.page = 1;
+    this.content = item.post;
+    this.time = item.time;
+    this.isShowTime = true;
+  }
   
 
 }
--- src/app/home/data/note-lowdb.service.ts
+++ src/app/home/data/note-lowdb.service.ts
@@ -9,13 +9,22 @@ export class NoteLowdbService {
   public FileSync = require('lowdb/adapters/FileSync');
   public adapter = new this.FileSync('src/dbs/note_db.json');
   public db = this.low(this.adapter);
-  public tmp_note = this.db.defaults({ week: [] }).write();
+  public tmp_note = this.db.defaults({ note: [] }).write();
 
   constructor() { }
 
   // 输入数据
   set(data: any) {
-    console.log("ok, come in.")
-    this.db.get('week').push(data).write();
+    this.db.get('note').push(data).write();
+  }
+
+  // 取数据
+  get() {
+    return this.db.get("note");
+  }
+
+  // 删除数据
+  remove_note(item: any) {
+    this.db.get('note').remove(item).write();
   }
 }
